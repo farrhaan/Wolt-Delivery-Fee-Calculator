4d83960bccc2255db89f3e80d59722c9fc29e6c0
M Farhan <m.ffarrhan@gmail.com>
2024-01-31 06:03:02 +0200

    :sparkles: styling config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Fonts/Omnes-Regular.ttf";
-import './App.css';
+import './App.scss';
+import CalculatorForm from './Components/CalculatorForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>WOLT Delivery Fee Calculator</h1>
+      <h1 className=''>WOLT Delivery Fee Calculator</h1>
+      <CalculatorForm />
     </div>
   );
 }
